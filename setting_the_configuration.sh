36d9daf664b0475a07a35a54e91a4fb06f08c6fb
WolfAURman <93985232+WolfAURman@users.noreply.github.com>
2022-03-25 20:50:19 +0600

    Update setting_the_configuration.sh

--- setting_the_configuration.sh
+++ setting_the_configuration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Updating data in repositories, and updating packages
-sudo apt update && upgrade
+sudo apt update && sudo apt upgrade
 
 #Install packages
 sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-11-jdk zram-config python
